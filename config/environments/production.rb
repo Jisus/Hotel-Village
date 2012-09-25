3d0bcfcae1d4fda4d19ca9d4bdb1adb505e2dea9
Rodrigo Gomes Dias <jisusbh@gmail.com>
2012-09-25 03:55:18 -0300

    Fechamento Sistema

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,5 +73,15 @@ Hotel::Application.configure do
     Devise::PasswordsController.layout "login"        
   end
   config.action_mailer.default_url_options = { :host => '187.33.0.175' }
-  
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'jisusbh@gmail.com',
+    password:             'rd2220casa',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
